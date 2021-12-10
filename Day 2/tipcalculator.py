c127ed47b053d0f6c363396e600e2d9833c58aec
Faiz Saiyad <81345756+Enhancifire@users.noreply.github.com>
2021-12-10 20:43:11 +0530

    Day 2/100 Completed

--- Day 2/tipcalculator.py
+++ Day 2/tipcalculator.py
@@ -1,1 +1,9 @@
-import numpy
+print("Welcome to the tip calculator.")
+bill = float(input("What was the total bill? $"))
+tip = float(input("What percentage tip would you like to give? 10, 12, 0r 15? "))
+people = float(input("How many people to split the bill? "))
+
+output = (bill / people) * tip/100
+output = (bill / people) + output
+
+print("Each person should pay: $" + str(round(output, 2)))
