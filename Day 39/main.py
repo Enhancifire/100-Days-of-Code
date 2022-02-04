307afe45dd0752b602043418cdbd24d85ffadd4b
Faiz Saiyad <81345756+Enhancifire@users.noreply.github.com>
2022-02-04 22:54:45 +0530

    Changed some stuff

--- Day 39/main.py
+++ Day 39/main.py
@@ -1,12 +1,15 @@
 import requests
 from twilio.rest import Client
+import json
 
 # res = requests.get(url="https://opentdb.com/api.php?amount=10").json()
 
 # print(res)
+with open('data.json') as f:
+    data = json.load(f)
 
-account_sid = 'AC46d0e80047812c5bf003f571c0e8ef50'
-auth_token = 'fd6e0f409140a67b0c51a82163e5f27b'
+account_sid = data['sid']
+auth_token = data['authToken']
 client = Client(account_sid, auth_token)
 
 message = client.messages \
